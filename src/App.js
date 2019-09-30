70193071e27683eff11afe74073d85da5cdacc39
root <root@localhost.localdomain>
2019-09-30 19:00:01 -0400

    first try
    Signed-off-by: root <root@localhost.localdomain>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import SpotifyPlayer from 'react-spotify-player';
+
+// size may also be a plain string using the presets 'large' or 'compact'
+const size = {
+  width: '100%',
+  height: 300,
+};
+const view = 'list'; // or 'coverart'
+const theme = 'black'; // or 'white'
+ 
+
+class App extends Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {artist: '', spotifyID : '7ocwZINHqO9DZJgFtc1B20'};
+		this._onClick = this._onClick.bind(this)
+		this._onChange = this._onChange.bind(this)
+	}
+	_onClick() {
+    this.setState({spotifyID:this.state.artist})
+		console.log('_onClick');
+ 	}
+
+	_onChange(event) {
+	   this.setState({artist:event.target.value}, () => console.log(this.state.artist))
+	}
+
+
+	render() {
+let urii="spotify:artist:".concat(this.state.spotifyID) 
+	  return (
+	    <div className="App">
+	      <header className="App-header">
+	  <div class="form-group">
+	    <label for="input">Artist Spotify ID : </label> 
+	    <input onChange={this._onChange} type="text" id="input" placeholder="artist spotify ID"/>
+	    <button onClick={this._onClick}>Search</button>
+	<hr/>
+	  </div>
+		<SpotifyPlayer
+		  uri={urii}
+		  size={size}
+		  view={view}
+		  theme={theme}
+		/>
+	      </header>
+	    </div>
+	  );
+	}
 }
 
 export default App;
